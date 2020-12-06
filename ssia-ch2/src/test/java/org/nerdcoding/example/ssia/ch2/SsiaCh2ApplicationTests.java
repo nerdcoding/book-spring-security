87b22504cd529fd2053857a40b466d477f539ab4
Tobias Koltsch <tobias@koltsch.de>
2020-12-06 17:13:57 +0100

    Create basic Spring Boot application for chap 2 examples

---
+++ ssia-ch2/src/test/java/org/nerdcoding/example/ssia/ch2/SsiaCh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nerdcoding.example.ssia.ch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
