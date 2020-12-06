5a2f6526d6ff2dc3aa8d0de3bdd854ce14aa6c60
Tobias Koltsch <tobias@koltsch.de>
2020-12-06 17:32:20 +0100

    Configure UserDetailsService and PasswordEncoder with configure

--- ssia-ch2/src/main/java/org/nerdcoding/example/ssia/ch2/config/SecurityConfig.java
+++ ssia-ch2/src/main/java/org/nerdcoding/example/ssia/ch2/config/SecurityConfig.java
@@ -18,19 +18,27 @@
 
 package org.nerdcoding.example.ssia.ch2.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
         var userDetailsService = new InMemoryUserDetailsManager();
         userDetailsService.createUser(
                 User.withUsername("bob")
@@ -39,11 +47,8 @@ public class SecurityConfig {
                         .build()
         );
 
-        return userDetailsService;
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
 }
