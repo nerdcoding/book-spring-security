96458dd733867b47cc936947b70a567fc8e7cde4
Tobias Koltsch <tobias@koltsch.de>
2020-12-19 16:44:31 +0100

    Use MVC matcher to authorize endpoints to roles.

---
+++ ssia-ch8/src/main/java/org/nerdcoding/example/ssia/ch8/SsiaCh8Application.java
@@ -0,0 +1,13 @@
+package org.nerdcoding.example.ssia.ch8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh8Application.class, args);
+    }
+
+}
