7556adcf8feb87479a286eeb29eadc842c237940
Tobias Koltsch <tobias@koltsch.de>
2020-12-19 10:39:07 +0100

    Add main products page and fix schema data types

---
+++ ssia-ch6/src/main/java/org/nerdcoding/example/ssia/ch6/persistence/entity/product/Product.java
@@ -0,0 +1,73 @@
+/*
+ * Product.java
+ *
+ * Copyright (c) 2020, Tobias Koltsch. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ */
+
+package org.nerdcoding.example.ssia.ch6.persistence.entity.product;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String price;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(final String price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(final Currency currency) {
+        this.currency = currency;
+    }
+}
---
+++ ssia-ch6/src/main/java/org/nerdcoding/example/ssia/ch6/service/ProductService.java
@@ -0,0 +1,41 @@
+/*
+ * ProductService.java
+ *
+ * Copyright (c) 2020, Tobias Koltsch. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ */
+
+package org.nerdcoding.example.ssia.ch6.service;
+
+import org.nerdcoding.example.ssia.ch6.persistence.entity.product.Product;
+import org.nerdcoding.example.ssia.ch6.persistence.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
