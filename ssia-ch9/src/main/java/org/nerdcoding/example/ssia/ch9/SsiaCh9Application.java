db7101521ee785685f68a5e09d3be3617158d919
Tobias Koltsch <tobias@koltsch.de>
2020-12-19 17:12:27 +0100

    Add a filter before authentication checking if a 'REQUEST_ID' header is present.

---
+++ ssia-ch9/src/main/java/org/nerdcoding/example/ssia/ch9/SsiaCh9Application.java
@@ -0,0 +1,13 @@
+package org.nerdcoding.example.ssia.ch9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh9Application.class, args);
+    }
+
+}
