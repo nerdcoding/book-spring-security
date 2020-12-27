15eb26303fdc617c496189db9a89b14a12d6dcfb
Tobias Koltsch <tobias@koltsch.de>
2020-12-27 15:17:23 +0100

    Add OAuth2 flow with GitHub as authorization server

---
+++ ssia-ch12/src/main/java/org/nerdcoding/example/ssia/ch12/SsiaCh12Application.java
@@ -0,0 +1,13 @@
+package org.nerdcoding.example.ssia.ch12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh12Application.class, args);
+    }
+
+}
---
+++ ssia-ch12/src/main/java/org/nerdcoding/example/ssia/ch12/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+/*
+ * SecurityConfig.java
+ *
+ * Copyright (c) 2020, Tobias Koltsch. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ */
+
+package org.nerdcoding.example.ssia.ch12.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.oauth2Login(configurer ->
+                configurer.clientRegistrationRepository(clientRegistrationRepository())
+        );
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(clientRegistration());
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB
+                .getBuilder("github")
+                .clientId("paste-client-id")
+                .clientSecret("paste-client-secret")
+                .build();
+    }
+
+}
