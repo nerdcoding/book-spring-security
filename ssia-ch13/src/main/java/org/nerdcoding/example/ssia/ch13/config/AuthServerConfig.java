63215b98b29f33a3c17825102facd896c902c987
Tobias Koltsch <tobias@koltsch.de>
2021-01-02 08:06:06 +0100

    Implement OAuth2 resource server which is using the authorization server

--- ssia-ch13/src/main/java/org/nerdcoding/example/ssia/ch13/config/AuthServerConfig.java
+++ ssia-ch13/src/main/java/org/nerdcoding/example/ssia/ch13/config/AuthServerConfig.java
@@ -25,6 +25,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -40,6 +41,11 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(final AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("isAuthenticated()");
     }
 
     @Override
@@ -63,7 +69,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .scopes("read")
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token")
                 .redirectUris("http://127.0.0.1:9090/home")
-                .autoApprove(true);
+                .autoApprove(true)
+            .and()
+                .withClient("resourceserver")
+                .secret("resourceserversecret");
     }
 }
 
