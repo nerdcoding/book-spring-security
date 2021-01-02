48bcdf37a9005064e2d277889da70a6e4fd13d32
Tobias Koltsch <tobias@koltsch.de>
2021-01-02 09:06:01 +0100

    Create JWT signed with a symmetric key used by auth & resource sever

--- ssia-ch15/resource-server/src/main/java/org/nerdcoding/example/ssia/ch15/resource/server/config/RessourceServerConfig.java
+++ ssia-ch15/resource-server/src/main/java/org/nerdcoding/example/ssia/ch15/resource/server/config/RessourceServerConfig.java
@@ -18,10 +18,42 @@
 
 package org.nerdcoding.example.ssia.ch15.resource.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
-public class RessourceServerConfig {
+public class RessourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final String jwtKey;
+
+    public RessourceServerConfig(@Value("${jwt.key}") final String jwtKey) {
+        this.jwtKey = jwtKey;
+    }
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtKey);
+        converter.setVerifierKey(jwtKey);
+
+        return converter;
+    }
 }
