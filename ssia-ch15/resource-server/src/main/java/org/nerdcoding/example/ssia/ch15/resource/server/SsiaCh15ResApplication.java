7641b318902e59d0c397d7babee845b59fd389dd
Tobias Koltsch <tobias@koltsch.de>
2021-01-02 08:33:11 +0100

    Add basic authorization & resource server

---
+++ ssia-ch15/resource-server/src/main/java/org/nerdcoding/example/ssia/ch15/resource/server/SsiaCh15ResApplication.java
@@ -0,0 +1,13 @@
+package org.nerdcoding.example.ssia.ch15.resource.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh15ResApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh15ResApplication.class, args);
+    }
+
+}
